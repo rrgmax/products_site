491d5620d9a26659734399b56ad8b9f7328d7816
Rogerio <rrgmax@bol.com.br>
2019-02-19 03:03:12 -0300

    Set home, produtos and clientes into views.py file

--- produtos/itens/views.py
+++ produtos/itens/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Área de Home</h1>')
+
+def produtos(request):
+    return HttpResponse('<h1>Área de produtos</h1>')
+
+def clientes(request):
+    return HttpResponse('<h1>Área de Clientes</h1>')
+
+    
+ 
