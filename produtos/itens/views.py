46a773dbfecebe83e8d5fc5695a364348d0ad5af
Rogerio <rrgmax@bol.com.br>
2019-02-19 02:48:48 -0300

    Set models.py file and run migrations

--- produtos/itens/views.py
+++ produtos/itens/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-class Produto(models.Model):
-    nome = models.CharField(max_length=200)
-    preco = models.DecimalField(max_digits=5, decimal_places=2)
-    descricao = models.TextField()
