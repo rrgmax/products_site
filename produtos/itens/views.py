d2b7f8155a733f8344570258d40d409b3b0e7792
Rogerio <rrgmax@bol.com.br>
2019-02-19 02:40:05 -0300

    Define Produtos class into views.py

--- produtos/itens/views.py
+++ produtos/itens/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    descricao = models.TextField()
